c54f94f5593318c50502dbf67cc6c329554c0689
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2023-03-30 18:07:42 +0300

    feat: migrate to Java 19, jakarta APIs, Spring Boot 3

--- src/main/java/org/ujar/bs/msg/kafka/producing/hello/config/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package org.ujar.bs.msg.kafka.producing.hello.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@EnableScheduling
-class ApplicationConfig {
-}
