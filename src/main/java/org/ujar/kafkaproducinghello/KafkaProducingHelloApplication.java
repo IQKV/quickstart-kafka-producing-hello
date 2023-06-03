e0b0b62724a7ad51d686e8b379a21a63bd98cf61
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2023-06-03 18:45:44 +0300

    chore: enable reasonable defaults for observability components
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

--- src/main/java/org/ujar/kafkaproducinghello/KafkaProducingHelloApplication.java
+++ src/main/java/org/ujar/kafkaproducinghello/KafkaProducingHelloApplication.java
@@ -2,10 +2,13 @@ package org.ujar.kafkaproducinghello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class KafkaProducingHelloApplication {
   public static void main(String[] args) {
-    SpringApplication.run(KafkaProducingHelloApplication.class, args);
+    SpringApplication springApplication = new SpringApplication(KafkaProducingHelloApplication.class);
+    springApplication.setApplicationStartup(new BufferingApplicationStartup(2048));
+    springApplication.run(args);
   }
 }
